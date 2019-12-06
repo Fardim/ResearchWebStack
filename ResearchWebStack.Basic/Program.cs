64df2b66bca38202a47e06a71887e2d308a80f1a
FARDIM KAISER <fardim_drmc_9094@yahoo.com>
2019-12-07 05:39:47 +0600

    first commit with Task 1 (a,b,c.d)

---
+++ ResearchWebStack.Basic/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Auk.CsharpBootstrapper.Extensions;
+using Auk.CsharpBootstrapper.Helper;
+using Auk.CsharpBootstrapper.Implementations.Converter;
+using log4net;
+using NUnit.Framework.Internal;
+using ResearchWebStack.BusinessLayer;
+using ResearchWebStack.DataModel;
+
+namespace ResearchWebStack.Basic
+{
+    class Program
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+        
+        static void Main(string[] args)
+        {
+            LogHelper.EnableDebugMode();
+            var configFilePath = PathHelper.GetPathCombineFromBase("Auk.CsharpBootstrapper.Example.log4net.config");
+            LogHelper.GetConfiguredLog4Net(configFilePath);
+            LogHelper.InjectLog4NetLogger(Log);
+
+            Serializer ser = new Serializer();
+            string path = string.Empty;
+            string xmlInputData = string.Empty;
+            string xmlOutputData = string.Empty;
+            
+            // EXAMPLE 1
+            path = Directory.GetCurrentDirectory() + @"\SampleTrxXMLTestLogFile.trx";
+            xmlInputData = File.ReadAllText(path);
+
+            TestRun testRun = ser.Deserialize<TestRun>(xmlInputData);
+            LogHelper.QInfo("Successfully parsed data from xml file.");
+
+
+            //UnitTestResults.UnitTestResultsAll(testRun.Results);
+            UnitTestResults.GetFailedTest(testRun);
+            //UnitTestResults.GetNonPassingTests(testRun);
+            //UnitTestResults.GetInfo(testRun, "(null,00000000-0000-0000-0000-000000000000,null)", (int) FilterTypeEnum.FilterType.Contains);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ResearchWebStack.BusinessLayer/UnitTestResults.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Auk.CsharpBootstrapper.Helper;
+using Newtonsoft.Json;
+using ResearchWebStack.DataModel;
+
+namespace ResearchWebStack.BusinessLayer
+{
+    public class UnitTestResults
+    {
+        public UnitTestResults()
+        {
+            
+        }
+
+        public static void UnitTestResultsAll(IList<TestRunResultsUnitTestResult> results)
+        {
+            var allResults = results.Select(re =>
+            {
+                LogHelper.QDebug(Newtonsoft.Json.JsonConvert.SerializeObject(re));
+                return Newtonsoft.Json.JsonConvert.SerializeObject(re);
+            }).ToArray();
+
+        }
+
+        public static void GetFailedTest(TestRun testRun)
+        {
+            var failedTests = testRun.Results.Where(r => r.outcome.Equals("NotExecuted")).Select(r =>
+            {
+                var obj = new
+                {
+                    TestName = r.testName,
+                    ClassName = testRun.TestDefinitions.FirstOrDefault(d => d.id == r.testId)?.TestMethod.FirstOrDefault()?.className,
+                    FullyQuantifiedName = new StringBuilder().Append(r.testId).Append(" - ").Append(r.testName).Append(" - ").Append(r.computerName),
+                    Status = r.outcome
+                };
+                LogHelper.QWarn(Newtonsoft.Json.JsonConvert.SerializeObject(obj));
+                return Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+            }).ToArray();
+        }
+
+        public static void GetNonPassingTests(TestRun testRun)
+        {
+            var failedTests = testRun.Results.Where(r => !r.outcome.Equals("Passed")).Select(r =>
+            {
+                var obj = new
+                {
+                    TestName = r.testName,
+                    ClassName = testRun.TestDefinitions.FirstOrDefault(d => d.id == r.testId)?.TestMethod.FirstOrDefault()?.className,
+                    FullyQuantifiedName = new StringBuilder().Append(r.testId).Append(" - ").Append(r.testName).Append(" - ").Append(r.computerName),
+                    Status = r.outcome
+                };
+                LogHelper.QDebug(Newtonsoft.Json.JsonConvert.SerializeObject(obj));
+                return Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+            }).ToArray();
+        }
+
+        public static void GetInfo(TestRun testRun, string testName, int filterType)
+        {
+            Func<string, bool> filterExp = FilterExpression(testName, filterType);
+            var infos = testRun.TestDefinitions.Select(t => t.TestMethod.Where(m => filterExp(m.name)).Select(d =>
+            {
+                LogHelper.QDebug(JsonConvert.SerializeObject(d));
+                return d;
+            }).ToArray()).ToArray();
+        }
+
+        private static Func<string, bool> FilterExpression(string testName, int filterType)
+        {
+            if (filterType == (int)FilterTypeEnum.FilterType.Contains)
+            {
+                return x => x.Contains(testName);
+            }
+            else if (filterType == (int)FilterTypeEnum.FilterType.StartsWith)
+            {
+                return x => x.StartsWith(testName);
+            }
+            else
+            {
+                return x => x.EndsWith(testName);
+            }
+        }
+    }
+}
